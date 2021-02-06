3ad772b23beceb609a870298e0cc05dcbec7f30b
DanielSBaumann <dr4wone@gmail.com>
2021-02-06 17:05:33 -0300

    crud com spring

--- conect/src/main/java/io/github/danielsbaumann/SaleApplication.java
+++ conect/src/main/java/io/github/danielsbaumann/SaleApplication.java
@@ -1,12 +1,42 @@
 package io.github.danielsbaumann;
 
+import io.github.danielsbaumann.domain.entity.Cliente;
+import io.github.danielsbaumann.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class SaleApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes) {
+        return args -> {
+
+            /*
+            Cliente cliente = new Cliente();
+            cliente.setNome("Daniel Baumann");
+            clientes.salvar(cliente);
+
+            Cliente cliente2 = new Cliente();
+            cliente2.setNome("Thomas Baumann");
+            clientes.salvar(cliente2);
+            */
+
+            clientes.salvar(new Cliente("Daniel Baumann"));
+            clientes.salvar(new Cliente("Thomas Baumann"));
+            clientes.salvar(new Cliente("Paula Bastos"));
+
+            List<Cliente> list = clientes.obterTodos();
+
+            list.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SaleApplication.class, args);
---
+++ conect/src/main/java/io/github/danielsbaumann/domain/repository/Clientes.java
@@ -0,0 +1,36 @@
+package io.github.danielsbaumann.domain.repository;
+
+import io.github.danielsbaumann.domain.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class Clientes {
+
+    private static String INSERT = "insert into cliente (nome) values (?)";
+    private static String SELECT_ALL = "SELECT * FROM CLIENTE";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void salvar(Cliente cliente) {
+        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+    }
+
+    public List<Cliente> obterTodos() {
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+                Integer id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                return new Cliente(id, nome);
+            }
+        });
+    }
+}
